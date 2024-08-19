00e3b33a5fda864f5d5e71ab88508314cd7015ac
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-08-20 01:50:57 +0530

    NavBar Complete

--- vista-restaurent/src/components/Navbar.jsx
+++ vista-restaurent/src/components/Navbar.jsx
@@ -63,6 +63,39 @@ const Navbar = () => {
     </ul>
   </div>
   <div className="navbar-end">
+  <button className="btn btn-ghost btn-circle">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-5 w-5"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+      </svg>
+    </button>
+    {/* {Cart items} */}
+    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle mr-3 lg:flex hidden items-center justify-center">
+        <div className="indicator">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+          <span className="badge badge-sm indicator-item">8</span>
+        </div>
+      </div>
+    {/* {btn} */}
     <a className="btn bg-green rounded-full px-6 text-white flex items-center gap-2"><BiPhoneCall /> Contact</a>
   </div>
       </div>
