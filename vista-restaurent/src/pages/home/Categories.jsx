b74ebe08dc387c644b05445b153422e7a8d62b5f
NavodMahima <gnmshihara@std.appsc.sab.ac.lk>
2024-08-22 23:53:50 +0530

    Categories-start

---
+++ vista-restaurent/src/pages/home/Categories.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../../components/Banner'
+
+const Categories = () => {
+  return (
+    <div>
+      Categories
+    </div>
+  )
+}
+
+export default Categories
